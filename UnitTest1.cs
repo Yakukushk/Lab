4829ca6634e94db8af2f715175a4e29909b42cd5
Yakukushk <75535844+Yakukushk@users.noreply.github.com>
2021-10-31 13:37:49 +0200

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,32 +1,62 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LabCalculator1;
+using LabCalculator;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Windows.Forms;
 
-namespace LabCalculator1.Tests
+
+namespace LabCalculator.Tests
 {
     [TestClass]
-    public class Form1Tests
+    public class UnitTest1
     {
-        [TestMethod()]
-        public void Form1Test()
+        public class CalculatorTests
         {
-            Form1 form = new Form1();
-
-            
-            Assert.AreEqual(form.getDataGridView().Columns[0].Name, "A");
-            Assert.AreEqual(form.getDataGridView().Columns[4].Name, "A4");
-            
-            
-            Assert.AreEqual(form.getDataGridView().Rows[0].HeaderCell.Value, "01");
-            Assert.AreEqual(form.getDataGridView().Rows[4].HeaderCell.Value, "04");
+            [TestMethod()]
+            public void EvaluateTestExponent()
+            {
+                Assert.AreEqual(Calculator.Evaluate("0 ^ 0"), 1);
+                Assert.AreEqual(Calculator.Evaluate("1 ^ 10"), 1);
+                Assert.AreEqual(Calculator.Evaluate("10 ^ 1"), 10);
+                Assert.AreEqual(Calculator.Evaluate("10 ^ 2"), 100);
+                Assert.AreEqual(Calculator.Evaluate("10 ^ 0"), 1);
+            }
         }
         [TestMethod()]
-       
-        public void TestMethod1()
+        public void EvaluateTestMinExpression()
         {
+            Assert.AreEqual(Calculator.Evaluate("min(12, 10)"), 10);
+            Assert.AreEqual(Calculator.Evaluate("min(1.2, 10)"), 1.2);
+            Assert.AreEqual(Calculator.Evaluate("min(1, 1)"), 1);
+            Assert.AreEqual(Calculator.Evaluate("min(1, 2)"), 1);
+            Assert.AreEqual(Calculator.Evaluate("min(20,23)"), 20);
+        }
+        [TestMethod()]
+        public void EvaluateTestMaxExpreContext()
+        {
+            Assert.AreEqual(Calculator.Evaluate("max(12, 10)"), 12);
+            Assert.AreEqual(Calculator.Evaluate("max(1.2, 10)"), 10);
+            Assert.AreEqual(Calculator.Evaluate("max(1, 1)"), 1);
+            Assert.AreEqual(Calculator.Evaluate("max(-1, 0)"), 0);
+            Assert.AreEqual(Calculator.Evaluate("max(20,-23)"), 20);
+        }
+        [TestMethod()]
+        public void EvaluateTestUnaryMinusExpr()
+        {
+            Assert.AreEqual(Calculator.Evaluate("1 - 2"), -1);
+            Assert.AreEqual(Calculator.Evaluate("33 - 21"), 12);
+            Assert.AreEqual(Calculator.Evaluate("11 - 21"), -10);
+            Assert.AreEqual(Calculator.Evaluate("2 - 1"), 1);
+            Assert.AreEqual(Calculator.Evaluate("1 - 1"), 0);
+        }
+        [TestMethod()]
+        public void EvaluateTestModExpression()
+        {
+            Assert.AreEqual(Calculator.Evaluate("12 mod 2"), 0);
+            Assert.AreEqual(Calculator.Evaluate("13 mod 2"), 1);
+            Assert.IsTrue(Math.Abs(Calculator.Evaluate("13.1 mod 2") - 1.1) < 0.1);
+            Assert.AreEqual(Calculator.Evaluate("0 mod 2"), 0);
+            Assert.AreEqual(Calculator.Evaluate("13 mod 2"), 1);
         }
     }
 }
